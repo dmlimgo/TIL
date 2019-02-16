03fbbf33899dca2d9ecb2b113c5658015f4e7f36
dmlimgo home <dongmyeong.lim@gmail.com>
2019-02-16 23:31:03 +0900

    190216 d3 1491

--- 1. PS/1.2 SW Expert Academy/D3/1491.py
+++ 1. PS/1.2 SW Expert Academy/D3/1491.py
@@ -1,3 +1,11 @@
 T = int(input())
 for tc in range(T):
     n, a, b = map(int, input().split())
+    m = (a + b) * n
+    for r in range(1, n+1):
+        for c in range(1, int(n/r)+1):
+            s = a * abs(r - c) + b * (n - r * c)
+            if s < m:
+                m = s
+    print(f'#{tc+1} {m}')
+
