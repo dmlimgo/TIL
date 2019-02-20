a1ef3dfb4d21d307fa79f8c259f4aec93114ba99
dmlimgo home <dongmyeong.lim@gmail.com>
2019-02-21 01:33:32 +0900

    190221 home

--- 1. PS/1.1 Backjoon/DFS/11403.py
+++ 1. PS/1.1 Backjoon/DFS/11403.py
@@ -1,11 +1,13 @@
 def dfs(v):
-    global N, n
-    n += 1
-    if n != 1:
+    global n, N
+
+    if n != 0:
         visit[v] = 1
+    n += 1
     for w in range(1, N+1):
         if arr[v][w] == 1 and visit[w] == 0:
             dfs(w)
+
 
 
 N = int(input())
@@ -16,4 +18,4 @@ for i in range(1, N+1):
     visit = [0 for _ in range(N + 1)]
     n = 0
     dfs(i)
-    print(visit[1:])
+    print(' '.join(map(str, visit[1:])))
