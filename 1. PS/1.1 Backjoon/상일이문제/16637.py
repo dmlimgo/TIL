b6cda1d0148c8e6d94c514126b5072fe165927cd
dmlimgo home <dongmyeong.lim@gmail.com>
2019-03-05 02:13:57 +0900

    190304 sangil

--- 1. PS/1.1 Backjoon/상일이문제/16637.py
+++ 1. PS/1.1 Backjoon/상일이문제/16637.py
@@ -1,6 +1,18 @@
 
 
+
+
+def solve(k, res):
+    print(k, res)
+    if k == N-1:
+        print(res)
+        return
+    if susik[k] == '+': solve(k+1, int(res) + int(susik[k+1]))
+    if susik[k] == '-': solve(k+1, int(res) - int(susik[k+1]))
+    if susik[k] == '*': solve(k+1, int(res) * int(susik[k+1]))
+    if susik[k].isnumeric() : solve(k+1, res)
+
+
 N = int(input())
 susik = list(input())
-
-cal()
+solve(0, susik[0])
