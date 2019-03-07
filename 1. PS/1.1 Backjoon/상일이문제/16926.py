20ee3bc862dbbea38e0c3e796472bdfa07918e5e
dmlimgo <dongmyeong.lim@gmail.com>
2019-03-07 17:51:16 +0900

    190307 sangil

---
+++ 1. PS/1.1 Backjoon/상일이문제/16926.py
@@ -0,0 +1,21 @@
+import sys
+sys.stdin = open('16926.txt')
+
+N, M, R = map(int, input().split())
+arr = [[0 for _ in range(M)] for _ in range(N)]
+
+for i in range(N):
+    arr[i] = list(map(int, input().split()))
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+def rotate():
+    for n in range(N):
+        for m in range(M):
+            if
+            narr[j][i]
+
+for i in range(R):
+    narr = [[0 for _ in range(M)] for _ in range(N)]
+    rotate()
--- 1. PS/1.1 Backjoon/상일이문제/practice.py
+++ 1. PS/1.1 Backjoon/상일이문제/practice.py
@@ -1,50 +1,6 @@
-import sys
-import copy
-
-sys.stdin = open('p.txt')
-def solve(res, new_exp):
-    global maxval
-    for i in res:
-        a = int(new_exp.pop(i-1))
-        op = new_exp.pop(i-1)
-        b = int(new_exp.pop(i-1))
-        if op == '+': new_exp.insert(i-1, a+b)
-        if op == '-': new_exp.insert(i-1, a-b)
-        if op == '*': new_exp.insert(i-1, a*b)
-    result = int(new_exp[0])
-    for i in range(1, len(new_exp), 2):
-        if new_exp[i] == '+': result += int(new_exp[i+1])
-        if new_exp[i] == '-': result -= int(new_exp[i+1])
-        if new_exp[i] == '*': result *= int(new_exp[i+1])
-    if maxval < result:
-        maxval = result
-
-
-def permutation(res):
-    global N
-    new_exp = copy.deepcopy(exp)
-    solve(res[::-1], new_exp)
-    for i in range(3, N, 2):
-        if not visit[i] and num[i] > res[-1] + 3:
-            visit[i] = 1
-            permutation(res + [num[i]])
-            visit[i] = 0
-
-
-N = int(input())
-exp = list(input())
-if N == 1:
-    print(exp[0])
-else:
-    N -= 1
-    num = list(range(N))
-    visit = [0] * N
-    maxval = -1000001
-    new_exp = copy.deepcopy(exp)
-    solve([1], new_exp)
-    for i in range(3, N, 2):
-        visit[i] = 1
-        permutation([i])
-        visit[i] = 0
-    print(maxval)
-
+import time
+now = time.time()
+a = [0,1,2]
+for i in range(1000):
+    narr = [[0 for _ in range(300)] for _ in range(300)]
+print(time.time()-now)
