ecc21e1e894f88fbd8747f4d39b34beb465ef593
dmlimgo home <dongmyeong.lim@gmail.com>
2019-03-11 00:16:07 +0900

    190310 a problem

--- 1. PS/1.2 SW Expert Academy/역량/test.py
+++ 1. PS/1.2 SW Expert Academy/역량/test.py
@@ -1,2 +1,14 @@
-for i in range(1, 0, -1):
+def test():
+    global i
+    if i == 30:
+        print('done')
+        return 0
+    else:
+        print(i,'def')
+        i += 1
+        return 1
+i = 0
+while test(): continue
+
+for i in range(99,-1,-1):
     print(i)
