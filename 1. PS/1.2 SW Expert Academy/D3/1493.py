83a461dd0eed22ee926f65b3e72100938ff64beb
dmlimgo <dongmyeong.lim@gmail.com>
2019-03-14 19:28:51 +0900

    190314 d3

---
+++ 1. PS/1.2 SW Expert Academy/D3/1493.py
@@ -0,0 +1,30 @@
+import sys
+sys.stdin = open('1493.txt')
+
+def getval(n):
+    s = 1
+    i = 1
+    while True:
+        if s <= n < s + i:
+            break
+        s += i
+        i += 1
+    x = 1 + (n - s)
+    y = i - (n - s)
+    return x, y
+
+def solve(x, y):
+    s = 0
+    for i in range(1, x+1):
+        s += i
+    for j in range(1, y+1):
+        s += i + j
+        i += j
+
+T = int(input())
+p, q = map(int, input().split())
+x1, y1 = getval(p)
+x2, y2 = getval(q)
+x = x1 + x2
+y = y1 + y2
+print(f'#{tc+1} {solve(x, y)}')
