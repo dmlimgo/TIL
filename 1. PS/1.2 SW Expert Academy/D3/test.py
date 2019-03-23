c65fd43c8579dbe73f244d6ce09330bb21177b35
dmlimgo home <dongmyeong.lim@gmail.com>
2019-03-23 22:00:43 +0900

    190323 d3 problem

--- 1. PS/1.2 SW Expert Academy/D3/test.py
+++ 1. PS/1.2 SW Expert Academy/D3/test.py
@@ -1,9 +1,11 @@
-def Bbit_print(i):
-    output = ''
-    for j in range(7, -1, -1):
-        output += '1' if i & (1 << j) else '0'
-    print(output)
-
-for i in range(-5, 6):
-    print( '%3d = ' % i, end='')
-    Bbit_print(i)
+import sys
+sys.stdin = open('t1.txt')
+a = []
+for i in range(1000):
+    a.append(input())
+sys.stdin = open('t2.txt')
+for i in range(1000):
+    if a[i] != input():
+        print(i,'wrong')
+    else:
+        print(i)
