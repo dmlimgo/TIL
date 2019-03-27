da9dcc10c387e20a988a221901935d08c71f14f2
dmlimgo <dongmyeong.lim@gmail.com>
2019-03-27 17:59:38 +0900

    190327 codexpert

--- 1. PS/1.3 Codeexpert/AD/2275_나무 자르기.py
+++ 1. PS/1.3 Codeexpert/AD/2275_나무 자르기.py
@@ -3,18 +3,22 @@ sys.stdin = open('2275.txt')
 import time
 start = time.time()
 def solve(m):
-    global para
     hap = 0
-    if para:
-        for i in range(N):
-            if m >= arr[i]:
-                continue
+    for i in range(N):
+        if m < arr[i]:
             hap += (arr[i] - m)
-    else:
-        for i in range(N):
-            if m >= arr[i]:
-                break
-            hap += (arr[i] - m)
+    # for i in range(N):
+    #     if m >= arr[i]:
+    #         break
+    #     hap += (arr[i] - m)
+    # print('func', end=' ')
+    # j = len(arr)
+    # for i in range(N):
+    #     if m >= arr[i]:
+    #         j = i
+    #         break
+    # print(j, m, sum(arr[:j]), m*j)
+    # hap = sum(arr[:j]) - m*j
     return hap
 
 def bs(s, e):
@@ -22,8 +26,15 @@ def bs(s, e):
     sol = -1
     while s <= e:
         m = (s + e) // 2
-        # print(s, m, e, solve(m))
-        if solve(m) >= M:
+        hap = solve(m)
+        # print(s, m, e, hap)
+        # print(time.time() - start)
+        # if hap == M:
+        #     return m
+        # elif hap > M:
+        #     sol = m
+        #     s = m + 1
+        if hap >= M:
             sol = m
             s = m + 1
         else:
@@ -32,12 +43,8 @@ def bs(s, e):
 
 N, M = map(int, input().split())
 arr = list(map(int, input().split()))
-para = 0
-if sum(arr) // 2 <= M:
-    arr.sort()
-    para = 1
-else:
-    arr.sort(reverse=True)
+# arr.sort(reverse=True)
+# print(time.time()-start)
 # print(arr)
 print(bs(0, max(arr)))
 print(time.time()-start)
--- 1. PS/1.3 Codeexpert/AD/practice.py
+++ 1. PS/1.3 Codeexpert/AD/practice.py
@@ -1,6 +1,10 @@
 import sys
-f = open('practice.txt', 'w')
-for i in range(1, 1000001):
-    data = str(i) + ' '
-    f.write(data)
-f.close()
+hap = 0
+for i in range(999998):
+   hap += i
+print(hap)
+# f = open('practice.txt', 'w')
+# for i in range(1, 1000001):
+#     data = str(i) + ' '
+#     f.write(data)
+# f.close()
