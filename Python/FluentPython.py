2cd19cd0e3903bcf2302148cc0a6a2a811678d79
dmlimgo <dongmyeong.lim@gmail.com>
2020-01-16 23:52:06 +0900

    FluentPython study

--- Python/FluentPython.py
+++ Python/FluentPython.py
@@ -56,19 +56,34 @@
 # print(a)
 
 # ex 2-11
-invoice = """
-0.....6.............................36........46...52....
-1909  Pimoroni PiBrella             $17.50    3    $52.50
-1489  6mm Tactile Switch x20         $4.95    2     $9.90
-1510  Panavise Jr. - PV-201         $28.00    1    $28.00
-1601  PiFTF Mini Kit 320x240        $34.95    1    $34.95
-"""
+# invoice = """
+# 0.....6.............................36........46...52....
+# 1909  Pimoroni PiBrella             $17.50    3    $52.50
+# 1489  6mm Tactile Switch x20         $4.95    2     $9.90
+# 1510  Panavise Jr. - PV-201         $28.00    1    $28.00
+# 1601  PiFTF Mini Kit 320x240        $34.95    1    $34.95
+# """
 
-SKU = slice(0, 6)
-DESCRIPTION = slice(6, 36)
-UNIT_PRICE = slice(36, 46)
-QUANTITY = slice(46, 51)
-ITEM_TOTAL = slice(51, None)
-line_items = invoice.split('\n')[2:-1]
-for item in line_items:
-    print(item[UNIT_PRICE], item[DESCRIPTION])
+# SKU = slice(0, 6)
+# DESCRIPTION = slice(6, 36)
+# UNIT_PRICE = slice(36, 46)
+# QUANTITY = slice(46, 51)
+# ITEM_TOTAL = slice(51, None)
+# line_items = invoice.split('\n')[2:-1]
+# for item in line_items:
+#     print(item[UNIT_PRICE], item[DESCRIPTION])
+
+# ex
+# l = list(range(10))
+# l[3:6] = [100]
+# print(l)
+# del l[::4]
+# print(l)
+
+# ex 2-12, 2-13
+row = ['_'] * 3
+board = []
+for i in range(3):
+    board.append(row)
+board[1][0] = '*'
+print(board)
